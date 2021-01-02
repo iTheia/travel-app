e740e2d14b28d298c5a4de0890cc93d12f0ecef2
Marlon <martinez.1ded@gmail.com>
2021-01-01 19:14:11 -0600

    db model

--- server/src/components/files/model.ts
+++ server/src/components/files/model.ts
@@ -1,29 +1,33 @@
 import {
-	Entity,
-	PrimaryGeneratedColumn,
-	Column,
-	BaseEntity,
-	OneToMany,
-} from 'typeorm';
-import { User } from '../users';
+    Entity,
+    PrimaryGeneratedColumn,
+    Column,
+    BaseEntity,
+    OneToMany,
+} from "typeorm";
+import { Post } from "../post";
+import { User } from "../users";
 
 @Entity()
 export class File extends BaseEntity {
-	@PrimaryGeneratedColumn()
-	id: number;
+    @PrimaryGeneratedColumn()
+    id: number;
 
-	@Column({ unique: true })
-	name: string;
+    @Column({ unique: true })
+    name: string;
 
-	@Column({ nullable: false })
-	type: string;
+    @Column({ nullable: false })
+    type: string;
 
-	@Column({ nullable: false })
-	size: string;
+    @Column({ nullable: false })
+    size: string;
 
-	@Column()
-	path: string;
+    @Column()
+    path: string;
 
-	@OneToMany(() => User, (userInfo) => userInfo.avatar)
-	users: User[];
+    @OneToMany(() => Post, (post) => post.image)
+    posts: User[];
+
+    @OneToMany(() => User, (userInfo) => userInfo.avatar)
+    users: User[];
 }
--- server/src/components/users/model.ts
+++ server/src/components/users/model.ts
@@ -1,22 +1,31 @@
 import {
-	Entity,
-	PrimaryGeneratedColumn,
-	Column,
-	BaseEntity,
-	JoinColumn,
-	ManyToOne,
-} from 'typeorm';
-import { File } from '../files';
+    Entity,
+    PrimaryGeneratedColumn,
+    Column,
+    BaseEntity,
+    JoinColumn,
+    ManyToOne,
+    OneToMany,
+} from "typeorm";
+import { Comment } from "../comment";
+import { File } from "../files";
+import { User_Post } from "../user_post";
 
 @Entity()
 export class User extends BaseEntity {
-	@PrimaryGeneratedColumn()
-	id: number;
+    @PrimaryGeneratedColumn()
+    id: number;
 
-	@Column({ nullable: true, default: 1 })
-	avatar_id: number;
+    @Column({ nullable: true, default: 1 })
+    avatar_id: number;
 
-	@ManyToOne(() => File, (file) => file.users)
-	@JoinColumn({ name: 'avatar_id' })
-	avatar: File;
+    @ManyToOne(() => File, (file) => file.users)
+    @JoinColumn({ name: "avatar_id" })
+    avatar: File;
+
+    @OneToMany(() => Comment, (comment) => comment.author)
+    comments: Comment[];
+
+    @OneToMany(() => User_Post, (user_post) => user_post.user)
+    favorites: User_Post[];
 }
