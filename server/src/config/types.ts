2709174d45746573b7bd7cc23e6de9bee9a03e99
Marlon <martinez.1ded@gmail.com>
2021-01-01 19:13:02 -0600

    host dir add

--- server/src/config/types.ts
+++ server/src/config/types.ts
@@ -1,16 +1,17 @@
 export interface IConfig {
-	port: number;
-	token: string;
-	allowedOrigin: string[];
-	refreshToken: string;
-	database: IDatabase;
+    port: number;
+    token: string;
+    allowedOrigin: string[];
+    refreshToken: string;
+    database: IDatabase;
+    host: string;
 }
 
 export interface IDatabase {
-	host: string;
-	port: number;
-	username: string;
-	type: 'mysql' | 'postgress';
-	password: string;
-	database: string;
+    host: string;
+    port: number;
+    username: string;
+    type: "mysql";
+    password: string;
+    database: string;
 }
